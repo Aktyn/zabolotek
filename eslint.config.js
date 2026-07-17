be5f76c27ef9b9448bc4465f750e628709595a34
Aktyn <Aktyn3@gmail.com>
2026-07-17 19:25:12 +0200

    Initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,80 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import prettier from "eslint-plugin-prettier"
+import eslintConfigPrettier from "eslint-config-prettier"
+import { defineConfig } from "eslint/config";
+import reactCompiler from "eslint-plugin-react-compiler"
+
+export default defineConfig(
+  { ignores: ["dist", "node_modules"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "tsconfig.app.json",
+        sourceType: "module",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettier,
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: [
+            "buttonVariants",
+            "badgeVariants",
+            "useFormField",
+            "useView",
+            "useQuickSettings",
+            "usePinnedDataStores",
+          ],
+        },
+      ],
+      "react-compiler/react-compiler": "error",
+      "prettier/prettier": "error",
+      eqeqeq: "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error", "info"],
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          fixStyle: "separate-type-imports",
+        },
+      ],
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "object-shorthand": ["error", "always"],
+      "react-hooks/set-state-in-effect": "off"
+    },
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      todo
+    </div>
+  )
+}
+
+export default App
